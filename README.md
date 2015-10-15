191be4da1e61f2d67ceaa8556ae3121fdd84e722
yeojoy <yeojoy.kim@gmail.com>
2015-10-15 14:35:41 +0900

    README.md 수정. email 보내기 추가

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 혈압과 혈당관리를 하기 위한 프로젝트로, 혈압(최대, 최소, 맥박)을 입력하고 혈당(혈당, 측정시각, 몸무게)를 입력하면  
 화면에서 입력한 data를 보여준다.
 
-Server를 따로 구성하지 않았으며 [parse.com](parse.com)을 사용해서 데이터를 저장하고 보여준다.
+Server를 따로 구성하지 않았으며 [parse.com](https://www.parse.com)을 사용해서 데이터를 저장하고 보여준다.
 
 # How to run
 현재 ApiKey라는 constants가 있는 interface가 .gitignore에 추가된 상태이다.  
 그러므로 Parse의 Application ID와 Client Key를 아래의 코드에 추가해야한다.
+
+URQA는 주석처리 하거나 삭제해도 상관없다.
 
 ```
 package me.yeojoy.foryou.config;
@@ -21,10 +23,9 @@ public interface ApiKey {
 추가해 줘야만 데이터를 저장하고 불러올 수 있다.
 
 # TODO
-- 입력한 data 수정기능.
-- graph를 line과 bar로 표현
 - local db(sqlite3)과 sync 기능
 
 # 3rd libraries
 - Good FAB 3rd library > [Floating Action Button](https://github.com/Clans/FloatingActionButton)
 - Amazing Graph library > [MPAndroidChart](https://github.com/PhilJay/MPAndroidChart)
+- URQA - bug reporting > [urqa](http://urqa.io/urqa/)
--- app/src/main/java/me/yeojoy/foryou/backup/BackupActivity.java
+++ app/src/main/java/me/yeojoy/foryou/backup/BackupActivity.java
@@ -1,6 +1,14 @@
 package me.yeojoy.foryou.backup;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
 
 import me.yeojoy.foryou.BaseActivity;
 import me.yeojoy.foryou.R;
@@ -12,39 +20,43 @@ public class BackupActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_backup);
 
-    }
+        // 버튼 객체
+        Button btnSendEmail = (Button) findViewById(R.id.btn_send_email);
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_input_blood_pressure) {
-            Intent intent = new Intent(mContext, InputActivity.class);
-            intent.putExtra(KEY_INPUT_TYPE, INPUT_TYPE_BLOOD_PRESSURE);
-            startActivity(intent);
-            return true;
-        } else if (id == R.id.action_input_blood_sugar) {
-            Intent intent = new Intent(mContext, InputActivity.class);
-            intent.putExtra(KEY_INPUT_TYPE, INPUT_TYPE_BLOOD_SUGAR);
-            startActivity(intent);
-            return true;
+        // 전송할 파일의 경로
+        String szSendFilePath = Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + "/test.jpg";
+        File f = new File(szSendFilePath);
+        if (!f.exists()) {
+            Toast.makeText(this, "파일이 없습니다.", Toast.LENGTH_SHORT).show();
         }
 
-        return super.onOptionsItemSelected(item);
+        // File객체로부터 Uri값 생성
+        final Uri fileUri = Uri.fromFile(f);
+
+        /** 첨부파일 이메일 보내기 */
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(Intent.ACTION_SEND);
+                it.setType("plain/text");
+
+                // 수신인 주소 - tos배열의 값을 늘릴 경우 다수의 수신자에게 발송됨
+                String[] tos = {"test@gmail.com"};
+                it.putExtra(Intent.EXTRA_EMAIL, tos);
+
+                it.putExtra(Intent.EXTRA_SUBJECT, "[혈압.혈당앱] 자료 백업 email.");
+                it.putExtra(Intent.EXTRA_TEXT, "고객님께서 저장하신 혈압.혈당 기록을 보내드립니다" +
+                        ".\n");
+
+                // 파일첨부
+                it.putExtra(Intent.EXTRA_STREAM, fileUri);
+
+                startActivity(it);
+            }
+        });
     }
-    */
+
 }
